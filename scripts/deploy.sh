274b154975bc12d45ca8572865de1022c82b8ed3
DutKise <labriochemoledu54@gmail.com>
2026-06-03 13:56:20 +0200

    Finalize StackNova deployment automation

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,18 +3,21 @@ set -e
 
 echo "=== Étape 1 : Création de l'infrastructure avec Terraform ==="
 
-cd terraform
-terraform init -upgrade
+cd "$(dirname "$0")/../terraform"
+
+terraform init
 terraform apply -auto-approve
 
 echo ""
 echo "=== Infrastructure créée. Attente de 3 secondes avant configuration... ==="
+
 sleep 3
 
 echo ""
 echo "=== Étape 2 : Configuration avec Ansible ==="
 
 cd ../ansible
+
 ansible-playbook -i inventaire.ini playbook.yml
 
 echo ""
